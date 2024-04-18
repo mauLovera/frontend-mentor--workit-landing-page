74d9b39f641cd7e07828a88c4ce61b09a3de864d
mauLovera <mauricioloveraedu@gmail.com>
2024-04-18 15:12:42 -0400

    update: serif font

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,4 +1,8 @@
-import { Fraunces, Manrope } from "next/font/google"
+import { Manrope } from "next/font/google"
+import localFont from "next/font/local"
 
-export const serif = Fraunces({ subsets: ["latin"], weight: ["600"] })
-export const sans = Manrope({ subsets: ["latin"], weight: ["400"] })
+export const serif = localFont({
+  src: "./Fraunces_144pt-SemiBold.ttf",
+  display: "swap",
+})
+export const sans = Manrope({ subsets: ["latin"], weight: ["400", "700"] })
