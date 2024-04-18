714755b84b762b9369c096e6920f5ca15c4a079f
mauLovera <mauricioloveraedu@gmail.com>
2024-04-18 15:13:23 -0400

    wip: header and background banners

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import type { Metadata } from "next";
+import type { Metadata } from "next"
 
-import "./globals.css";
-import { sans } from "@/fonts";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import "./globals.css"
+import { sans } from "@/fonts"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Workit Landing Page",
   description: "Data tailored to your needs",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${sans.className} flex min-h-screen flex-col bg-white px-4 md:pt-14 pt-8 sm:px-10 md:px-[160px]`}
+        className={`${sans.className} flex min-h-screen flex-col bg-white px-4 pt-8 text-body sm:px-10 md:px-20 md:pt-14 xl:px-[160px]`}
       >
+        <div className="absolute left-0 top-0 -z-10 h-[481px] w-screen bg-primary sm:h-[540px] md:h-[730px]" />
+        <div className="absolute left-0 top-[392px] -z-20 h-[1070.71px] w-screen bg-off-white sm:top-[403px] sm:h-[920.73px] md:top-[504px] md:h-[930px]" />
         <Header />
         {children}
         <Footer />
       </body>
     </html>
-  );
+  )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,11 @@
+import { ButtonOrLink } from "./ButtonOrLink"
+import Logo from "./icons/Logo"
+
 export default function Header() {
-	return <header>Header</header>
+  return (
+    <header className="flex w-full justify-between">
+      <Logo />
+      <ButtonOrLink intent={"secondary"}>Apply for access</ButtonOrLink>
+    </header>
+  )
 }
