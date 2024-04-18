0d2d914c5ae12e5ee33f6b4709910a67082c6134
mauLovera <mauricioloveraedu@gmail.com>
2024-04-18 12:24:34 -0400

    feat: responsive global padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next"
+import type { Metadata } from "next";
 
-import "./globals.css"
-import { sans } from "@/fonts"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
+import "./globals.css";
+import { sans } from "@/fonts";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-	title: "Frontend Mentor | Workit Landing Page",
-	description: "Data tailored to your needs",
-}
+  title: "Frontend Mentor | Workit Landing Page",
+  description: "Data tailored to your needs",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={sans.className}>
-				<Header />
-				{children}
-				<Footer />
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body
+        className={`${sans.className} flex min-h-screen flex-col bg-white px-4 md:pt-14 pt-8 sm:px-10 md:px-[160px]`}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
