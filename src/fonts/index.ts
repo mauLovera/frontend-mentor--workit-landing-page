9bb9e51799f370b5ff6b390e257ba51023c10da0
mauLovera <mauricioloveraedu@gmail.com>
2024-04-18 11:51:52 -0400

    feat: add assets, fonts && tw-config

---
+++ src/fonts/index.ts
@@ -0,0 +1,4 @@
+import { Fraunces, Manrope } from "next/font/google"
+
+export const serif = Fraunces({ subsets: ["latin"], weight: ["600"] })
+export const sans = Manrope({ subsets: ["latin"], weight: ["400"] })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		colors: {
+			primary: "hsl(273, 85%, 13%)",
+			accent: "hsl(150, 100%, 63%)",
+			neutral: "hsl(271, 12%, 34%)",
+			"off-white": "hsl(274, 100%, 99%)",
+			white: "hsl(0, 0%, 100%)",
+		},
+		fontSize: {
+			"heading-lg": ["80px", "80px"],
+			"heading-md": ["56px", "64px"],
+			"heading-sm": ["32px", "40px"],
+			body: ["18px", "32px"],
+		},
+	},
+	plugins: [],
+}
+export default config
