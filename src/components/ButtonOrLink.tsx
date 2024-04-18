ccf76721297534c1e8f43e5c3be5a74eec7b41bf
mauLovera <mauricioloveraedu@gmail.com>
2024-04-18 15:13:51 -0400

    feat: button component with cva

---
+++ src/components/ButtonOrLink.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link"
+import { cva, type VariantProps } from "class-variance-authority"
+
+const buttonOrLinkStyles = cva(
+  "flex items-center justify-center font-bold transition-all duration-300 tracking-[0.18px]",
+  {
+    variants: {
+      intent: {
+        primary:
+          "bg-accent text-primary hover:text-accent hover:bg-primary border-2 border-accent pt-[13px] pb-4 px-[25px]",
+        secondary: "text-white border-b-[3px] border-accent hover:text-accent",
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+    },
+  },
+)
+
+export interface Props extends VariantProps<typeof buttonOrLinkStyles> {
+  children: React.ReactNode
+  href?: string
+  onClick?: () => void
+}
+
+export function ButtonOrLink({
+  intent,
+  children,
+  href,
+  onClick,
+  ...props
+}: Props) {
+  return href ? (
+    <Link href={href} className={buttonOrLinkStyles({ intent })} {...props}>
+      {children}
+    </Link>
+  ) : (
+    <button
+      className={buttonOrLinkStyles({ intent })}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
