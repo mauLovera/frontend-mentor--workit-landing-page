a162ad1558a480f018959533b0d5aa24bffd1da9
mauLovera <mauricioloveraedu@gmail.com>
2024-04-18 15:13:57 -0400

    feat: add hero

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,34 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		colors: {
-			primary: "hsl(273, 85%, 13%)",
-			accent: "hsl(150, 100%, 63%)",
-			neutral: "hsl(271, 12%, 34%)",
-			"off-white": "hsl(274, 100%, 99%)",
-			white: "hsl(0, 0%, 100%)",
-		},
-		fontSize: {
-			"heading-lg": ["80px", "80px"],
-			"heading-md": ["56px", "64px"],
-			"heading-sm": ["32px", "40px"],
-			body: ["18px", "32px"],
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      primary: "hsl(273, 85%, 13%)",
+      accent: "hsl(150, 100%, 63%)",
+      neutral: "hsl(271, 12%, 34%)",
+      "off-white": "hsl(274, 100%, 99%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontSize: {
+      "heading-lg": ["80px", "80px"],
+      "heading-md": ["56px", "64px"],
+      "heading-sm": ["32px", "40px"],
+      body: ["18px", "32px"],
+    },
+    backgroundImage: {
+      "banner-dark-mobile": "url('/images/banner-dark-mobile.svg')",
+      "banner-dark-tablet": "url('/images/banner-dark-tablet.svg')",
+      "banner-dark-desktop": "url('/images/banner-dark-desktop.svg')",
+      "banner-light-mobile": "url('/images/banner-light-mobile.svg')",
+      "banner-light-tablet": "url('/images/banner-light-tablet.svg')",
+      "banner-light-desktop": "url('/images/banner-light-desktop.svg')",
+    },
+  },
+  plugins: [],
 }
 export default config
